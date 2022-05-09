484e429d61d5a19a97b6f424dc43b3727094a53f
Orizk <105179124+Orizk@users.noreply.github.com>
2022-05-09 02:49:47 +0200

    Update covid_producer.py

--- APPS/Kafka/App_Covid/covid_producer.py
+++ APPS/Kafka/App_Covid/covid_producer.py
@@ -4,7 +4,7 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='192.168.33.13:9092')
 with open('data_covid.csv') as file:
     while line := file.readline(): 
-        producer.send('covid1', line.rstrip().encode('utf-8'))
+        producer.send('covid', line.rstrip().encode('utf-8'))
         time.sleep(1)
         
 
